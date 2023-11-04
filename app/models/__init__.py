5b0fa8570a609585ba2e36bd304fa0bb54d03be8
Nichita Gutu <gutunichita757@gmail.com>
2023-11-04 13:05:47 +0100

    initial models for DB

---
+++ app/models/__init__.py
@@ -0,0 +1,6 @@
+from .user import User
+from .word import Word
+from .definition import Definition
+from .user_word import UserWord
+from .user_definition import UserDefinition
+from .user_word_review import UserWordReview
